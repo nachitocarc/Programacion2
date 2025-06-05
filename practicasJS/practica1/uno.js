8fe08b81ededdad250e83cc6cc78a1cbf337d9be
Nachito <ignacio.fleita@alumnos.ispi4038.edu.ar>
2025-06-05 18:18:24 -0300

    prog

---
+++ practicasJS/practica1/uno.js
@@ -0,0 +1,59 @@
+var x = 0;
+
+
+//// punto 1
+function validar() {
+    var campo = document.getElementById("nombre")
+    if (campo.value == "") { 
+        alert("esta vacio"); }
+    else { 
+        alert("tiene letras"); }
+
+}
+
+/// punto 2
+
+function mostrarOcultar(){
+    var lorem = document.getElementById("contenedor");
+    var boton = document.getElementById("boton")
+    if (lorem.style.display == "none"){
+        lorem.style.display = "block"; boton.innerHTML= "ocultar";
+    }    
+    else {lorem.style.display = "none"; boton.innerHTML = "mostrar";
+    };
+}
+
+/// punto 3 
+
+function cambiarColor(){
+    var color = document.getElementById("color");
+    document.body.style.backgroundColor = color.value
+}
+
+/// punto 4
+
+function clickeo(){
+    var contador = document.getElementById("contador")
+    x += 1;
+    contador.innerText = x
+}
+
+/// punto 5 
+
+function hora(){
+    var p_hora = document.getElementById("hora")
+    var fecha = Date();
+    p_hora.innerHTML = fecha;
+}
+
+/// punto 6
+
+function cambiarTexto(){
+    var texto_nuevo =document.getElementById("texto_nuevo");
+    var texto_inicial = document.getElementById("texto_inicial");
+    texto_inicial.innerText = texto_nuevo.value;
+}
+
+
+
+
