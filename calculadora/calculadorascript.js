7c3bea5bb213dae643fe6b0a7c39a7cced09e9f6
Nachito <ignacio.fleita@alumnos.ispi4038.edu.ar>
2025-06-05 20:06:43 -0300

    calcu

---
+++ calculadora/calculadorascript.js
@@ -0,0 +1,21 @@
+var valor_a = 0;
+var valor_b = 0;
+let valor_final = document.getElementById("resultado")
+
+function agregarNumero(){
+
+}
+
+function operacion(){
+
+}
+
+function limpiar(){
+    valor_final.innerText = ""
+    valor_a = ""
+    valor_b = ""
+}
+
+function resultado(){
+
+}
