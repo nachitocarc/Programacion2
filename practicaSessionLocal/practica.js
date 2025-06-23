4df3b56666bca8416bf3e450478dbae4384f78c5
Nachito <ignacio.fleita@alumnos.ispi4038.edu.ar>
2025-06-23 14:18:02 -0300

    ejercicio 2 terminado y 3 empezado

--- practicaSessionLocal/practica.js
+++ practicaSessionLocal/practica.js
@@ -1,3 +1,7 @@
+/*
+Primero
+*/
+
 function guardarNombre(){
     localStorage.nombre = document.getElementById("ingreso_nombre").value;
 }
@@ -10,3 +14,33 @@ else {
 alert("No has introducido tu nombre");
 }
 }
+
+/*
+Segundo
+*/
+function visitas(){
+      let visitas = localStorage.getItem("visitas");
+            if (visitas == null) {
+                visitas = 1;
+            } else {
+                visitas = parseInt(visitas) + 1;
+            }
+
+            localStorage.setItem("visitas", visitas);
+
+            document.getElementById("contador_visitas").innerHTML = 
+                "Has visitado esta página " + localStorage.visitas + " veces.";
+        }
+
+        visitas();
+
+/*
+Tercero
+*/
+
+function cambiarColor(){
+    localStorage.color = document.getElementById("color").value;
+    if (localStorage.color != undefined)
+    {document.body.style.backgroundColor = localStorage.color}
+}
+
