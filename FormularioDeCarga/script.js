e8efe34434872b409a87dd295790d60b16972552
Nachito <ignacio.fleita@alumnos.ispi4038.edu.ar>
2025-06-23 19:45:42 -0300

    formularioDeCarga

---
+++ FormularioDeCarga/script.js
@@ -0,0 +1,43 @@
+
+
+function mostrarInformacion() {
+    let nombre_obtenido = document.getElementById("nombre")
+    let edad_obtenida = document.getElementById("edad")
+    let mail_obtenido = document.getElementById("mail")
+
+    let contenedor_nuevo = document.getElementById("contenedor_nuevo");
+
+    var muestra_nombre = document.createElement("p")
+    var muestra_edad = document.createElement("p")
+    var muestra_mail = document.createElement("p")
+    var nuevo_boton = document.createElement("input")
+
+
+    nuevo_boton.setAttribute("type", "button")
+    nuevo_boton.setAttribute("onclick", "volver()")
+    nuevo_boton.setAttribute("value", "apreta")
+
+
+    var nombre_final = document.createTextNode(nombre_obtenido.value)
+    var edad_final = document.createTextNode(edad_obtenida.value)
+    var mail_final = document.createTextNode(mail_obtenido.value)
+
+
+    muestra_nombre.appendChild(nombre_final)
+    muestra_edad.appendChild(edad_final)
+    muestra_mail.appendChild(mail_final)
+
+    contenedor_nuevo.appendChild(muestra_nombre)
+    contenedor_nuevo.appendChild(muestra_edad)
+    contenedor_nuevo.appendChild(muestra_mail)
+    contenedor_nuevo.appendChild(nuevo_boton)
+
+
+    document.getElementById("contenedor_form").style.display = "none";
+
+}
+
+function volver(){
+    document.getElementById("contenedor_form").style.display = "block";
+    document.getElementById("contenedor_nuevo").style.display = "none";
+}
--- practicaSessionLocal/practica.js
+++ practicaSessionLocal/practica.js
@@ -1,46 +1,42 @@
 /*
 Primero
 */
-
 function guardarNombre(){
-    localStorage.nombre = document.getElementById("ingreso_nombre").value;
+    let nombre_persona = document.getElementById("ingreso_nombre").value
+    localStorage.setItem("nombre", nombre_persona)
 }
 
 function recuperarNombre(){
-    if (localStorage.nombre != undefined)
-        {
-alert(localStorage.nombre)}
-else {
-alert("No has introducido tu nombre");
-}
+    const nombre_recibido = localStorage.getItem("nombre")
+    alert(nombre_recibido)
 }
 
 /*
 Segundo
 */
 function visitas(){
-      let visitas = localStorage.getItem("visitas");
-            if (visitas == null) {
-                visitas = 1;
-            } else {
-                visitas = parseInt(visitas) + 1;
-            }
-
-            localStorage.setItem("visitas", visitas);
-
-            document.getElementById("contador_visitas").innerHTML = 
-                "Has visitado esta página " + localStorage.visitas + " veces.";
-        }
-
-        visitas();
+    let visitas = localStorage.getItem("visitas");
+    if (visitas == null) {
+        visitas = 1;
+    } else {
+        visitas = parseInt(visitas) + 1;
+    }
+    localStorage.setItem("visitas", visitas);
+    document.getElementById("contador_visitas").innerHTML = 
+        "Has visitado esta página " + visitas + " veces.";
+}
+visitas();
 
 /*
 Tercero
 */
+const color_nuevo = localStorage.getItem("color_pagina");
+document.body.style.backgroundColor = color_nuevo
 
-function cambiarColor(){
-    localStorage.color = document.getElementById("color").value;
-    if (localStorage.color != undefined)
-    {document.body.style.backgroundColor = localStorage.color}
+function cambiarColor() {
+    let color_ventana = document.getElementById("color").value;
+    localStorage.setItem("color_pagina", color_ventana);
+    document.body.style.backgroundColor = color_nuevo;
 }
+
 
